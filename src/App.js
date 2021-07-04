8f4c712da502d7f67b285902822b0da676497197
Nguyễn Thanh Tùng <nttung99qn@gmail.com>
2021-07-04 17:34:52 +0700

    #added form add user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Home from './pages/Home/Home';
 import UserList from './pages/UserList/UserList';
 import User from './pages/User/User';
+import NewUser from './pages/NewUser/NewUser';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -23,6 +24,9 @@ function App() {
           </Route>
           <Route path="/user/:userId">
             <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
